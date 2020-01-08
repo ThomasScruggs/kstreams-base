3c3f545e24dcc7e2dc880afc3a02f09c4d58503a
Rohit Thadani <rthadani@plume.com>
2020-01-08 15:14:26 -0800

    Adding codebuild file

---
+++ codebuild/create-sbt-creds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+artifactory_url=${1}
+artifactory_user=${2}
+artifactory_pw=${3}
+
+mkdir -p ~/.sbt
+chmod 755 ~/.sbt
+echo "realm=Artifactory Realm" > ~/.sbt/.credentials
+echo "host=${artifactory_url}" >> ~/.sbt/.credentials
+echo "user=${artifactory_user}" >> ~/.sbt/.credentials
+echo "password=${artifactory_pw}" >> ~/.sbt/.credentials
