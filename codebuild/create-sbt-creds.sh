f689a4c9e895cbc3c13fe5fc5e8ac102be789531
Rohit Thadani <rthadani@plume.com>
2020-01-16 16:28:02 -0800

    resetting bad version

--- codebuild/create-sbt-creds.sh
+++ codebuild/create-sbt-creds.sh
@@ -3,12 +3,11 @@ artifactory_url=${1}
 artifactory_user=${2}
 artifactory_pw=${3}
 
+set -e
+
 mkdir -p ~/.sbt
 chmod 755 ~/.sbt
 echo "realm=Artifactory Realm" > ~/.sbt/.credentials
 echo "host=${artifactory_url}" >> ~/.sbt/.credentials
 echo "user=${artifactory_user}" >> ~/.sbt/.credentials
 echo "password=${artifactory_pw}" >> ~/.sbt/.credentials
-
-
-cat ~/.sbt/.credentials
